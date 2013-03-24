14bece9e3fcfc6ba2e3d0bc6d9648ddaedeb7ae3
Suan-Aik Yeo <yeosuanaik@gmail.com>
2013-03-24 13:54:45 -0500

    add tests for .convert()

--- test/instant-markdown-d_test.js
+++ test/instant-markdown-d_test.js
@@ -6,16 +6,14 @@ var subject   = require('../lib/instant-markdown-d'),
 chai.use(require('sinon-chai'));
 chai.should();
 
-beforeEach(function(){
-  sandbox = sinon.sandbox.create();
-});
-afterEach(function(){
-  sandbox.restore();
-});
+beforeEach(function(){ sandbox = sinon.sandbox.create(); });
+afterEach(function(){ sandbox.restore(); });
+
+
 
 describe('instant-markdown-d', function(){
 
-  describe('#start', function(){
+  describe('.start', function(){
     var execStub;
     beforeEach(function(){
       execStub = sandbox.stub(child_process, 'exec', function(){});
@@ -25,10 +23,37 @@ describe('instant-markdown-d', function(){
       subject.start(8090, 'gfm');
       execStub.should.have.been.calledWith('open -g http://localhost:8090');
     });
+
     it('starts server listening on specified port', function(){
       var serverListenStub = sandbox.stub(require('http').Server.prototype, 'listen', function(){});
       subject.start(8090, 'gfm');
       serverListenStub.should.have.been.called;
     });
   });
+
+
+  describe('.convert', function(){
+    var converter,
+        sock,
+        converterRender,
+        sockEmit;
+
+    beforeEach(function(){
+      converter = { render: function(){ return '<em>str</em>'; } };
+      converterRender = sandbox.spy(converter, 'render');
+      sock = { emit: function(){} };
+      sockEmit = sandbox.spy(sock, 'emit');
+    });
+
+    it("sends converted markup as 'newContent' on the websocket", function(){
+      subject.convert('*str*', converter, sock);
+      sockEmit.should.have.been.calledWith('newContent', '<em>str</em>');
+    });
+
+    it("passes raw markup to converter#render", function(){
+      subject.convert('*str*', converter, sock);
+      converterRender.should.have.been.called;
+    });
+  });
+
 });
