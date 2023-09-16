50f6c35651eb0061fb83abeda6ba03f39772f873
Ashwin V. Mohanan <dev@fluid.quest>
2023-09-16 23:31:12 +0200

    Update node packages and nix dependencies

--- default.nix
+++ default.nix
@@ -7,14 +7,16 @@ pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [
       nodePackages.npm
+      git
     ];
     # libs
     buildInputs = with pkgs; [
       nodejs-slim-12_x
-      git
     ];
     shellHook = ''
-      npm install -g
+      npm install .
+      ln -rsf ./src/cli.js node_modules/.bin/instant-markdown-d
+      export PATH="$PWD/node_modules/.bin:$PATH"
     '';
 }
 
