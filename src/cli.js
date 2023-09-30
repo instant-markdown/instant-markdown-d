056cffde6499143d0befc2ece08c3791d86a1faf
Ashwin V. Mohanan <dev@fluid.quest>
2023-09-30 15:46:25 +0200

    Adjust tests to rerun less

--- src/cli.js
+++ src/cli.js
@@ -32,7 +32,7 @@ const MarkdownIt = require('markdown-it'),
 const mjpage = require('mathjax-node-page').mjpage;
 const taskLists = require('markdown-it-task-lists');
 const frontMatter = require('markdown-it-front-matter');
- 
+
 if (argv.version || argv.debug) {
   const version= require('./version');
   console.log(`instant-markdown-d version: v${version}`);
@@ -85,7 +85,7 @@ let md = new MarkdownIt({
 }).use(taskLists, {enabled: true}).use(frontMatter, function(fm){});
 
 if (argv.mathjax) md.use(require('markdown-it-mathjax')());
-if (argv.mermaid) md.use(require('markdown-it-textual-uml'));
+if (argv.mermaid)  md.use(require('markdown-it-textual-uml'));
 
 const mjPageConfig = {
   format: ["TeX"],
